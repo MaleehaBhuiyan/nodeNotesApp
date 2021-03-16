210ac819021ac28c70ded4032f0d2dca662b992c
MaleehaBhuiyan <maleeha.bhuiyan@baruchmail.cuny.edu>
2021-03-16 13:52:08 -0400

    Yargs

--- app.js
+++ app.js
@@ -1,13 +1,61 @@
 const chalk = require('chalk')
+const yargs = require('yargs')
 const getNotes = require("./notes.js") // getting notes.js file 
 
-const command = process.argv[2]
 
-if(command === 'add'){
-    console.log('Adding Note')
-} else if (command === 'remove'){
-    console.log("Removing Note")
-}
+// customize yargs 
+yargs.version('1.1.0')
 
-console.log(process.argv)
+// Create add command 
+yargs.command({
+    command: 'add',
+    describe: 'Add a new note',
+    builder: {
+        title: {
+            describe: 'Note Title',
+            demandOption: true,
+            type: 'string'
+        },
+        body: {
+            describe: 'Note Body',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler: function(argv){
+        console.log("Title: " + argv.title)
+        console.log("Body: " + argv.body)
+    }
+})
+
+// Create remove command 
+yargs.command({
+    command: 'remove',
+    describe: 'Remove a note',
+    handler: function(){
+        console.log("Removing a note")
+    }
+})
+
+yargs.command({
+    command: 'read',
+    describe: 'Read a note',
+    handler: function(){
+        console.log("Reading a note")
+    }
+})
+
+yargs.command({
+    command: "list",
+    describe: "List a note",
+    handler: function(){
+        console.log("Listing a note")
+    }
+})
+
+// add, remove, read, list
+
+yargs.parse()
+
+
 
