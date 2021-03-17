be559aaecbf0dc1e3dc65bec6e4ee9cd2e7cc27b
MaleehaBhuiyan <maleeha.bhuiyan@baruchmail.cuny.edu>
2021-03-16 21:16:35 -0400

    Removing a note

--- notes.js
+++ notes.js
@@ -1,4 +1,6 @@
 const fs = require('fs')
+const chalk = require('chalk')
+
 const getNotes = function(){
     return 'Your notes...'
 }
@@ -17,10 +19,27 @@ const addNote = function(title, body) {
         })
     
         saveNotes(notes)
-        console.log("New Note Add")
+        console.log("New Note Added")
     } else {
         console.log("Note Title Taken")
     }
+}
+
+const removeNote = function(title){
+    const notes = loadNotes()
+
+    const notesToKeep = notes.filter(note => {
+        return note.title !== title    
+    })
+
+    if(notes.length !== notesToKeep.length){
+        console.log(chalk.green.inverse("Note Removed!"))
+    } else {
+        console.log(chalk.red.inverse("No Note Found!"))
+    }
+
+    saveNotes(notesToKeep)
+
 }
 
 const saveNotes = function (notes) {
@@ -38,7 +57,9 @@ const loadNotes = function () {
     }
 }
 
+
 module.exports = {
     getNotes: getNotes,
-    addNote: addNote
+    addNote: addNote,
+    removeNote: removeNote
 }
